cbf7e97130cae052615375e50ed9c00bcdcc4072
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-20 04:52:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,4 +33,8 @@ class TicTacToe
   def position_taken?(index)
     !(@board[index].nil? || @board[index] == " ")
   end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
 end
